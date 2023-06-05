2243eb029ccc4dce0fcd9970561e1fb30be02d01
Mark Duarte <duarte.mark8@gmail.com>
2023-06-05 16:51:43 -0700

    added many more cheeses

--- src/data/cheeses.js
+++ src/data/cheeses.js
@@ -15,6 +15,53 @@ const cheeses = [
     name: "feta",
     imagePath: "/images/feta.jpeg",
   },
+  {
+    name: "muenster",
+    imagePath: "/images/muenster.jpeg",
+  },
+  {
+    name: "cottage",
+    imagePath: "/images/cottage.jpeg",
+  },
+  {
+    name: "swiss",
+    imagePath: "/images/swiss.jpeg",
+  },
+  {
+    name: "american",
+    imagePath: "/images/american.jpeg",
+  },
+  {
+    name: "pepper jack",
+    imagePath: "/images/pepper-jack.jpeg",
+  },
+  {
+    name: "mozzarella",
+    imagePath: "/images/mozzarella.jpeg",
+  },
+  {
+    name: "provolone",
+    imagePath: "/images/provolone.jpeg",
+  },
+  {
+    name: "ricotta",
+    imagePath: "/images/ricotta.jpeg",
+  },
+  {
+    name: "pecorino",
+    imagePath: "/images/pecorino.jpeg",
+  },
+  {
+    name: "roquefort",
+    imagePath: "/images/roquefort.jpeg",
+  },
+  {
+    name: "gouda",
+    imagePath: "/images/gouda.jpeg",
+  },
+  {
+    name: "brie",
+    imagePath: "/images/brie.jpeg",
+  },
 ];
-
 export default cheeses;
